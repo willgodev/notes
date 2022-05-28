0430bb44ece752348c09d1a903bb2502d0672dd5
willgodev <willgodev@gmail.com>
2022-05-28 18:08:23 -0400

    Added labels for tiles

--- testgame.py
+++ testgame.py
@@ -15,6 +15,8 @@ SCREEN_DIMENSIONS = (SCREEN_WIDTH, SCREEN_HEIGHT)
 NUM_ROWS = SCREEN_HEIGHT // TILE_HEIGHT
 NUM_COLS = SCREEN_WIDTH // TILE_WIDTH
 
+FONT_SIZE = 12
+
 
 def draw_tiles():
     return
@@ -22,18 +24,31 @@ def draw_tiles():
 
 def main():
     pg.init()
+    pg.font.init()
+    default_font = pg.font.SysFont("arial", FONT_SIZE)
+
     screen = pg.display.set_mode(SCREEN_DIMENSIONS)
     pg.display.set_caption("Test")
 
     background = pg.Surface(screen.get_size()).convert()
     background.fill('black')
 
+    tile_labels = {}
+
+    tile_label = None
     for i in range(NUM_ROWS):
         for j in range(NUM_COLS):
-            target_top = i * TILE_HEIGHT
-            target_left = j * TILE_WIDTH
+            # tile_number = (i * NUM_COLS) + j
+
+            # Draw Tile
+            target_top = (i * TILE_HEIGHT)
+            target_left = (j * TILE_WIDTH)
             new_tile = pg.Rect((target_left, target_top), TILE_DIMENSIONS)
             pg.draw.rect(background, 'gray', new_tile)
+
+            # Draw Text
+            tile_label = pg.font.Font.render(default_font, "A", False, 'blue')
+            tile_labels[(target_left, target_top)] = tile_label
 
     is_running = True
     while is_running:
@@ -44,6 +59,10 @@ def main():
                 is_running = False
 
         screen.blit(background, (0, 0))
+
+        for tile_loc, tile_label in tile_labels.items():
+            screen.blit(tile_label, tile_loc)
+
         pg.display.flip()
 
     pg.quit()
