54ffb970f76416efcfaa5ca8c2f4acd14f594b67
willgodev <willgodev@gmail.com>
2022-06-05 21:53:06 -0400

    Move into functions, add mouse handling

--- testgame.py
+++ testgame.py
@@ -27,17 +27,41 @@ print(NUM_MINES)
 FONT_SIZE = 21
 
 
-def draw_tiles():
-    return
-
-
 def main():
     pg.init()
     pg.font.init()
     default_font = pg.font.SysFont("arial", FONT_SIZE)
 
+    def draw_tiles():
+        for i in range(NUM_ROWS):
+            for j in range(NUM_COLS):
+                # tile_number = (i * NUM_COLS) + j
+                tile_label = None
+
+                # Draw Tile
+                target_top = (i * TILE_HEIGHT)
+                target_left = (j * TILE_WIDTH)
+                new_tile = pg.Rect((target_left, target_top), TILE_DIMENSIONS)
+                pg.draw.rect(background, 'gray', new_tile)
+
+                # Draw Text
+                if board_state[i][j] == '*':
+                    tile_label = pg.font.Font.render(default_font, "*", False, 'blue')
+                    tile_labels[(target_left + (TILE_WIDTH // 4), target_top + (TILE_HEIGHT // 4))] = tile_label
+
+    def set_mines():
+        for _ in range(NUM_MINES):
+            mine_row = random.randint(0, NUM_ROWS-1)
+            mine_col = random.randint(0, NUM_COLS-1)
+            board_state[mine_row][mine_col] = '*'
+
+    def pos_to_tile_index(pos):
+        row = pos[1] // TILE_WIDTH
+        col = pos[0] // TILE_HEIGHT
+        return (row, col)
+
     screen = pg.display.set_mode(SCREEN_DIMENSIONS)
-    pg.display.set_caption("Test")
+    pg.display.set_caption("Minesweeper")
 
     background = pg.Surface(screen.get_size()).convert()
     background.fill('black')
@@ -46,26 +70,8 @@ def main():
 
     board_state = [[''] * NUM_COLS for _ in range(NUM_ROWS)]
 
-    for i in range(NUM_MINES):
-        mine_row = random.randint(0, NUM_ROWS-1)
-        mine_col = random.randint(0, NUM_COLS-1)
-        board_state[mine_row][mine_col] = '*'
-
-    tile_label = None
-    for i in range(NUM_ROWS):
-        for j in range(NUM_COLS):
-            # tile_number = (i * NUM_COLS) + j
-
-            # Draw Tile
-            target_top = (i * TILE_HEIGHT)
-            target_left = (j * TILE_WIDTH)
-            new_tile = pg.Rect((target_left, target_top), TILE_DIMENSIONS)
-            pg.draw.rect(background, 'gray', new_tile)
-
-            # Draw Text
-            if board_state[i][j] == '*':
-                tile_label = pg.font.Font.render(default_font, "*", False, 'blue')
-                tile_labels[(target_left + (TILE_WIDTH // 4), target_top + (TILE_HEIGHT // 4))] = tile_label
+    set_mines()
+    draw_tiles()
 
     is_running = True
     while is_running:
@@ -74,6 +80,8 @@ def main():
                 is_running = False
             elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
                 is_running = False
+            elif event.type == pg.MOUSEBUTTONDOWN:
+                selected_tile = pos_to_tile_index(pg.mouse.get_pos())
 
         screen.blit(background, (0, 0))
 
